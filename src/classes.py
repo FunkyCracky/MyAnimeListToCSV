248c69213c89a397e9b9baff1f4618ac9eecf512
FunkyCracky <futebol4@hotmail.com>
2021-12-18 23:37:53 +0000

    Manga fetching support added; General improvements

---
+++ src/classes.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+class Data(ABC):
+    @abstractmethod
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_element(self):
+        pass
+
+    def get_header(self):
+        return self.header
+
+    def get_size(self):
+        return len(self.table)   
+
+    def set_table(self, table):
+        self.table = table
+
+class AnimeData(Data):
+    def __init__(self) -> None:
+        super().__init__()
+        self.status = {1: "Watching", 2: "Completed", 3: "On Hold", 4: "Dropped", 6: "PTW"}
+        self.header = ["Title", "Status", "Score", "Eps. watched", "Start date", "End date", "Season", "Year", "Genres"]
+
+    def get_element(self, i):
+        return[self.table[i]["title"],
+               self.status[self.table[i]["watching_status"]],
+               self.table[i]["score"] if self.table[i]["score"] != 0 else "",
+               self.table[i]["watched_episodes"],
+               self.table[i]["watch_start_date"][:10] if self.table[i]["watch_start_date"] != None else "", 
+               self.table[i]["watch_end_date"][:10] if self.table[i]["watch_end_date"] != None else "",
+               str(self.table[i]["season_name"]) + " " + str(self.table[i]["season_year"]) if self.table[i]["season_year"] != None else "",
+               self.table[i]["season_year"] if self.table[i]["season_year"] != None else "",
+               get_genres(self.table[i])]
+
+class MangaData(Data):
+    def __init__(self) -> None:
+        super().__init__()
+        self.status = {1: "Reading", 2: "Completed", 3: "On Hold", 4: "Dropped", 6: "PTR"}
+        self.header = ["Title", "Status", "Score", "Chaps. read", "Vols. read", "Start date", "End date", "Type", "Genres"]
+
+    def get_element(self, i):
+        return[self.table[i]["title"],
+               self.status[self.table[i]["reading_status"]],
+               self.table[i]["score"] if self.table[i]["score"] != 0 else "",
+               self.table[i]["read_chapters"],
+               self.table[i]["read_volumes"],
+               self.table[i]["start_date"][:10] if self.table[i]["start_date"] != None else "", 
+               self.table[i]["end_date"][:10] if self.table[i]["end_date"] != None else "",
+               self.table[i]["type"],
+               get_genres(self.table[i])]
+
+def get_genres(el):
+    genres = ""
+    for genre in el["genres"]:
+        genres += genre["name"] + ", "
+    return genres[:-2]
---
+++ src/main.py
@@ -0,0 +1,52 @@
+from time import sleep
+from classes import AnimeData, MangaData
+import requests, csv, json, os
+
+def write_to_csv(data, csv_writer):
+    csv_writer.writerow(data.get_header())
+    for i in range(data.get_size()):
+        csv_writer.writerow(data.get_element(i))
+
+def fetch_and_write_data(out, data, csv_writer, page, username, c, m_type):
+    r = requests.get("https://api.jikan.moe/v3/user/{}/{}?page={}".format(username, m_type, page), timeout=6)
+    if r.status_code != 200:
+        print("Something went wrong. Code: " + str(r.status_code))
+        out.close()
+        return main()
+    table = json.loads(r.content)["anime" if c == "A" else "manga"]
+    data.set_table(table)
+    write_to_csv(data, csv_writer)
+
+def get_type():
+    c = str(input("Anime (A) or manga (M)? ")).upper()
+    info = {"A": ("animelist", AnimeData()), "M": ("mangalist", MangaData())}
+    if c in ("A", "M"):
+        return c, info[c]
+    else:
+        print("The input is incorrect. Try again!")
+        return get_type()
+
+def main():
+    username = str(input("MyAnimeList username: "))
+    c, (m_type, data) = get_type()
+    page = 1
+    out_name = str(input("File output name: "))
+    out_path = os.path.join(os.path.dirname( __file__ ), "..", out_name)
+    out = open(out_path + ".csv", "w", newline = "")
+    csv_writer = csv.writer(out, delimiter =";")
+    while True:
+        try:
+            fetch_and_write_data(out, data, csv_writer, page, username, c, m_type)
+            if data.get_size() < 300:
+                break
+            page += 1
+        except requests.exceptions.Timeout:
+            for i in range(4, 0, -1):
+                print("The API didn't respond. Trying again in " + str(i) + " seconds...", end = "\r")
+                sleep(1)
+            print("\nLet's try again!")
+    print("Done! Go to the main folder and check the file " + out_name + ".csv!")
+    out.close()
+
+if __name__ == "__main__":
+    main()
